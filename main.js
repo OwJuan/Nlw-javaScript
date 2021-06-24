c9591572df9d8ba2002a332236cef48e443fbd85
OwJuan <Juan.defreitas20@gmail.com>
2021-06-24 14:06:21 -0300

    Utilizando bibliotecas js, em novas section da aplicação

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-/* Abre e fecha Menu*/ 
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+/* Abre e fecha Menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-for(const element of toggle) {
-  element.addEventListener('click',  function () {
+for (const element of toggle) {
+  element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
 }
 
 /* Esconde o menu e vai para o item selecionado */
 
-const links = document.querySelectorAll('nav ul li a');
+const links = document.querySelectorAll('nav ul li a')
 
-for (const link of links){
-  link.addEventListener('click', function() {
-    nav.classList.remove('show');
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
 
@@ -23,8 +23,8 @@ for (const link of links){
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-  if (window.scrollY >= navHeight){
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
     //scroll é maior
     header.classList.add('scroll')
   } else {
@@ -32,3 +32,32 @@ window.addEventListener('scroll', function(){
     header.classList.remove('scroll')
   }
 })
+// TESTEMONIALS SWIPER
+
+const swiper = new Swiper('.swiper-container', {
+  slidePerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+// SCROLLREVEAL: Mostrar elementos quando der scroll na página
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 2000,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services .header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
